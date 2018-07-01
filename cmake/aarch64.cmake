de3905d696affc7411992e8e815baf953acbef44
Jonathan Glanz <jonathan.glanz@gmail.com>
2018-06-30 20:45:28 -0700

    example works!

---
+++ cmake/aarch64.cmake
@@ -0,0 +1,43 @@
+# CONFIGURE ENV
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+# DETERMINE THE CORRECT CROSS COMPILER
+if ("${LOCAL_AARCH64_FILE}" STREQUAL "")
+  set(LOCAL_AARCH64_FILE ${CMAKE_CURRENT_LIST_DIR}/../local_aarch64.cmake)
+endif()
+
+message(STATUS "Loading: ${LOCAL_AARCH64_FILE}")
+if(EXISTS ${LOCAL_AARCH64_FILE})
+  include(${LOCAL_AARCH64_FILE})
+  message("Loaded: ${LOCAL_AARCH64_FILE}")
+else()
+  message("USING DEFAULTS")
+  if(EXISTS /projects/oss/x-tools/aarch64-unknown-linux-gnu)
+    set(TOOLCHAIN_BASE /projects/oss/x-tools/aarch64-unknown-linux-gnu)
+    SET(TOOLCHAIN_DIR ${TOOLCHAIN_BASE})
+    SET(CROSS_COMPILE /projects/oss/x-tools/aarch64-unknown-linux-gnu/bin/aarch64-unknown-linux-gnu-)
+    SET(CROSS_COMPILE_CC ${CROSS_COMPILE}gcc)
+    SET(CROSS_COMPILE_CXX ${CROSS_COMPILE}g++)
+  elseif(EXISTS /usr/bin/aarch64-linux-gnu-gcc-6)
+    SET(TOOLCHAIN_BASE /usr/lib/gcc-cross/aarch64-linux-gnu)
+    SET(TOOLCHAIN_DIR ${TOOLCHAIN_BASE}/6.4.0)
+    SET(CROSS_COMPILE /usr/bin/aarch64-linux-gnu-)
+    SET(CROSS_COMPILE_CC ${CROSS_COMPILE}gcc-6)
+    SET(CROSS_COMPILE_CXX ${CROSS_COMPILE}g++-6)
+  else()
+    message(FATAL "NO COMPILER FOUND FOR AARCH64")
+  endif()
+endif()
+
+SET(CMAKE_C_COMPILER  ${CROSS_COMPILE_CC})
+SET(CMAKE_CXX_COMPILER ${CROSS_COMPILE_CXX})
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+SET(CMAKE_SIZEOF_VOID_P 8)
+
+
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+#include <google/protobuf/util/json_util.h>
+#include <google/protobuf/empty.pb.h>
+#include <google/protobuf/any.h>
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  //std::cout << "Hello, World: " << std::endl;
+  //google::protobuf::util::JsonParseOptions opts;
+  auto e = new google::protobuf::Empty();
+  printf("E2 size: %zu", e->ByteSizeLong());
+  //std::cout << "Byte size: " << e->ByteSizeLong() << std::endl;
+  //std::cout << "Hello, World: " << google::protobuf::internal::GetEmptyString() << std::endl;
+
+
   return 0;
 }
