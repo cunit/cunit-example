e0d27ab45fa59b638e48b9beebe9e581252b12e4
Jonathan Glanz <jonathan.glanz@gmail.com>
2018-07-01 19:50:38 -0700

    - revised the example to include FFMPEG

--- main.cpp
+++ main.cpp
@@ -9,23 +9,25 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+}
+
 int main() {
-  //std::cout << "Hello, World: " << std::endl;
-  //google::protobuf::util::JsonParseOptions opts;
-  //auto e = new google::protobuf::Empty();
-  //printf("E2 size: %zu", e->ByteSizeLong());
-  //std::cout << "Byte size: " << e->ByteSizeLong() << std::endl;
-  //std::cout << "Hello, World: " << google::protobuf::internal::GetEmptyString() << std::endl;
 
-  cv::Mat mat = cv::imread("/tmp/sample.png");
+  // CHECK FFMPEG
+  avcodec_register_all();
+  AVCodec * codec = av_codec_next(NULL);
+  while(codec != NULL) {
+    std::cout << codec->long_name << std::endl;
+    codec = av_codec_next(codec);
+  }
+
+  // CHECK OPENCV
+  cv::Mat mat = cv::imread("./sample.png");
   cv::Mat matOut(400,400,CV_8UC3);
-  std::cout << "Rows: " << mat.rows << std::endl;
-
   cv::resize(mat,matOut, cv::Size(), 0.75, 0.75);
-
   cv::imwrite("/tmp/sample4.jpeg",matOut);
-  //cv::imshow("sample",mat);
-  //cv::waitKey();
 
   return 0;
 }
